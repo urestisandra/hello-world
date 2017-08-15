f6d3d5278979af56add8ab05c1056a96edad406f
urestisandra <30833530+urestisandra@users.noreply.github.com>
2017-08-15 14:45:56 -0400

    Update ShipTest.java
    added this piece of code to the ShipTest class
    
    protected void run() {
            	double r = Math.random();
            	if (r < 0.1) {
                	fail("oops");
            	} else if (r < 0.2) {
               	throw new AssumptionViolatedException("skipping");
            	}
       	 }

--- javadoctest/src/test/java/com/rccl/entities/test/ShipTest.java
+++ javadoctest/src/test/java/com/rccl/entities/test/ShipTest.java
@@ -17,7 +17,14 @@ import org.junit.internal.AssumptionViolatedException;
  *
  */
 public class ShipTest {
-
+	protected void run() {
+        	double r = Math.random();
+        	if (r < 0.1) {
+            	fail("oops");
+        	} else if (r < 0.2) {
+           	throw new AssumptionViolatedException("skipping");
+        	}
+   	 }
 	/**
 	 * @throws java.lang.Exception
 	 */
